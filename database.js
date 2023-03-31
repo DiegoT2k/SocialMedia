5e06cda3c429d65bae4425dbfbfef442c5f4e4b4
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2023-03-31 16:32:21 +0200

    Check

--- database.js
+++ database.js
@@ -4,7 +4,6 @@
 //On subsequent calls, it will return the same instance that was 
 //created and returned to you the first time because of how module 
 //import/export works in ES6.
-//prova
 const mongoose = require("mongoose");
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
