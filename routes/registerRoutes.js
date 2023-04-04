b61d5802af3df4fc41e37984b55ae8f38921b072
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2023-04-04 12:43:01 +0200

    Edit register
    Remove (only comment) 'last name' and 'email' from register page

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -19,19 +19,20 @@ router.get("/", (req, res, next) => {
 router.post("/", async (req, res, next) => {
 
     var firstName = req.body.firstName.trim();
-    var lastName = req.body.lastName.trim();
+    //-var lastName = req.body.lastName.trim();
     var username = req.body.username.trim();
-    var email = req.body.email.trim();
+    //-var email = req.body.email.trim();
     var password = req.body.password;
 
     var payload = req.body;
 
-    if(firstName && lastName && username && email && password) 
+    if(firstName //- && lastName && email
+     && username && password) 
     {
         var user = await User.findOne({
             $or: [
                 { username: username },
-                { email: email }
+                //- { email: email }
             ]
         })
         .catch((error) => {
@@ -60,12 +61,12 @@ router.post("/", async (req, res, next) => {
         else 
         {
             // User found
-            if (email == user.email) {
-                payload.errorMessage = "Email already in use.";
-            }
-            else {
-                payload.errorMessage = "Username already in use.";
-            }
+        //- if (email == user.email) {
+        //-      payload.errorMessage = "Email already in use.";
+            //_}
+            //-else {
+               //- payload.errorMessage = "Username already in use.";
+            //-}
             res.status(200).render("register", payload);
         }
         
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: false, trim: true },
     username: { type: String, required: true, trim: true, unique: true },
-    email: { type: String, required: true, trim: true, unique: true },
+    email: { type: String, required: false, trim: true, unique: true },
     password: { type: String, required: true },
     profilePic: { type: String, default: "/images/profilePic.jpeg" },
     coverPhoto: { type: String },
